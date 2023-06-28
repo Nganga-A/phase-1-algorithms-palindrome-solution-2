6a617bb117a7535aaee8eb943be717c5d1250d8a
Abed Nganga <ngangaabed@gmail.com>
2023-06-29 00:44:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  const reversedStr = str.split('').reverse().join('');
+  return str === reversedStr;
 }
 
 /* 
   Add your pseudocode here
+
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
+
 */
 
 /*
   Add written explanation of your solution here
+
+  our solution consists os a function that checks if the first letter is the same as the last letter, and the second letter is the same as the second to last letter, and so on (until the middle of the word), the function should return true
 */
 
 // You can run `node index.js` to view these console logs
